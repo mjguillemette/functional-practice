0fa70751193b42d5017331ee71303847ead5613b
John Carmichael <jlcarmic@gmail.com>
2019-09-28 17:29:15 -0400

    Answers

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function calculateTimeSpent(chores) {
-  return chores
+  return chores.map((chore) => {
+    return { ...chore, timeSpent: chore.minutes * chore.timesDone }
+  })
 }
 
 function filterForQA(logs) {
-  return logs
+  return logs.filter((log) => log.environment === 'qa')
 }
 
 function filterByName(chores, name) {
-  return chores
+  return chores.filter((chore) => chore.doneBy === name)
 }
 
 function assignTasks(tasks, from, to) {
-  return tasks
+  return tasks.map((task) => {
+    if (task.assignee === from) {
+      return { ...task, assignee: to }
+    } else {
+      return task
+    }
+  })
 }
 
 module.exports = {
